cedc1205ffdefb1985ac46e3c8159c37a4408269
umohit35 <154299252+umohit35@users.noreply.github.com>
2024-01-29 09:50:07 +0530

    Delete 01_basics directory

--- 01_basics/02_datatypes.js
+++
@@ -1,5 +0,0 @@
-"use strict"; // treat all JS code as newer version of JS
-
-// alert(); // we are using node JS ,not a browser
-
-console.log();
--- 01_basics/06_num_and_maths.js
+++
@@ -1,6 +0,0 @@
-const min = 10;
-const max = 20;
-
-console.log(Math.random()); //generates a random number between 0 & 9
-
-console.log(Math.floor(Math.random() * (max - min + 1)) + min);
